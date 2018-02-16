54190ef3cceea6e1cde7680406f0a91136999627
Eric Solarz <esolarz@umich.edu>
2018-02-16 11:32:49 -0500

    Passing first test

--- rpn.py
+++ rpn.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python
 """RPN."""
 
+
 def calculate(arg):
-    pass
+    """Calculate."""
+    stack = list()
+    for token in arg.split():
+        try:
+            value = int(token)
+            stack.append(value)
+        except ValueError:
+            arg1 = stack.pop()
+            arg2 = stack.pop()
+            return arg1 + arg2
+
 
 def main():
-    """Main func."""
+    """Run main function."""
     while True:
         calculate(input('rpn calc> '))
 
