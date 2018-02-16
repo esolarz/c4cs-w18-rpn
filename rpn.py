9899be1c6b4d273b08ec54357ee14b3cf2f1ff2f
Eric Solarz <esolarz@umich.edu>
2018-02-16 11:17:33 -0500

    Add a makefile

---
+++ rpn.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+"""RPN."""
+
+def calculate(arg):
+    pass
+
+def main():
+    """Main func."""
+    while True:
+        calculate(input('rpn calc> '))
+
+
+if __name__ == "__main__":
+    main()
