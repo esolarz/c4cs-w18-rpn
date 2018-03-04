1bdb6fa54744c35a068fab18d885a7b55ca9844c
Eric Solarz <esolarz@umich.edu>
2018-03-04 14:14:53 -0500

    Add docstring to test

--- test_rpn.py
+++ test_rpn.py
@@ -2,6 +2,7 @@ import unittest
 import rpn
 
 class TestBasics(unittest.TestCase):
+    """Add docstring."""
     def test_add(self):
         result = rpn.calculate('1 1 +')
         self.assertEqual(2, result)
